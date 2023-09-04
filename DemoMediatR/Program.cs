bc69bcb447c1ee2024f93b8f1ae153e919644ec6
Hihiz <public@mail.ru>
2023-09-04 23:23:39 +0300

    Подключение NuGet пакетов, создание моделей, класс контекста

--- DemoMediatR/Program.cs
+++ DemoMediatR/Program.cs
@@ -1,4 +1,12 @@
+using DemoMediatR.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConnectionSqlite"));
+});
 
 // Add services to the container.
 
